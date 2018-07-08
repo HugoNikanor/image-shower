59b9142b7048b698edc23b4d012f427c64f16322
Hugo Hörnquist <hugo@hornquist.se>
2018-07-08 04:49:01 +0200

    Improvements to database schema.

--- db/create.sql
+++ db/create.sql
@@ -1,26 +1,34 @@
 -- For postgresql
+
+CREATE TYPE ptype AS ENUM
+    ('photo', 'text', 'video');
 	
 CREATE TABLE entries (
-	id INT PRIMARY KEY NOT NULL,
-	title     TEXT NOT NULL,
+	id SERIAL PRIMARY KEY NOT NULL,
+    post_id   BIGINT UNIQUE,
+	title     TEXT,
+    slug      TEXT,
 	text      TEXT,
 	media_url TEXT,
 	image_alt TEXT,
-	timestamp TIMESTAMP
+	timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    post_type ptype NOT NULL
 );
 
+COMMENT ON COLUMN entries.post_id   IS 'Old ID from Tumblr, will probably be removed';
 COMMENT ON COLUMN entries.title     IS 'Post Title!';
-COMMENT ON COLUMN entries.text      IS 'Extra text in post';
+COMMENT ON COLUMN entries.slug      IS 'post-title';
+COMMENT ON COLUMN entries.text      IS 'Extra text in post, might contain HTML';
 COMMENT ON COLUMN entries.media_url IS 'filename.png';
 COMMENT ON COLUMN entries.image_alt IS 'HTML image alt.';
 	
 CREATE TABLE tags (
-	id   INT PRIMARY KEY NOT NULL,
+	id   SERIAL PRIMARY KEY NOT NULL,
 	text TEXT NOT NULL UNIQUE
 );
 	
 CREATE TABLE tag_map (
-	id INT PRIMARY KEY NOT NULL,
+	id SERIAL PRIMARY KEY NOT NULL,
 	entry_id INT NOT NULL REFERENCES entries(id),
 	tag_id   INT NOT NULL REFERENCES tags(id),
     UNIQUE (entry_id, tag_id)
