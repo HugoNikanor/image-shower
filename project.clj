e33543a79269617502aa110260e33870120ce226
Hugo Hörnquist <hugo@hornquist.se>
2018-07-15 23:03:50 +0200

    Remove old honey database attempt.

--- project.clj
+++ project.clj
@@ -12,8 +12,6 @@
                  [garden "1.3.5"]       ; CSS
                  [korma "0.4.3"]        ; SQL
                  [org.postgresql/postgresql "9.4.1207"]
-                 [org.clojure/java.jdbc "0.7.7"]
-                 [honeysql "0.9.3"]
                  ]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler image-shower.core/handler
