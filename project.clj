099234fd9195a09b94b657efe2e04b947cae2d93
Hugo Hörnquist <hugo@hornquist.se>
2018-07-16 18:39:10 +0200

    Minor change in how ring is imported.

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [ring/ring-defaults "0.3.1"] ; contains ring/core
-                 [ring/ring-jetty-adapter "1.7.0-RC1"]
-                 [ring/ring-codec "1.1.1"]
+                 [ring "1.6.1"]
+                 [ring/ring-defaults "0.3.1"]
                  [compojure "1.6.1"]    ; defroutes
                  [hiccup "1.0.5"]       ; HTML
                  [garden "1.3.5"]       ; CSS
