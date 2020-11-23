8dcb8e70e28cbe38fd4fc4ac02ef39f95bfa2799
Hugo Hörnquist <hugo@lysator.liu.se>
2020-11-23 02:26:53 +0100

    Bump korma version

--- project.clj
+++ project.clj
@@ -9,13 +9,14 @@
                  [compojure "1.6.1"]    ; defroutes
                  [hiccup "1.0.5"]       ; HTML
                  [garden "1.3.5"]       ; CSS
-                 [korma "0.4.3"]        ; SQL
+                 [korma "0.5.0-RC1"]        ; SQL
                  [org.postgresql/postgresql "9.4.1207"]
                  [org.xerial/sqlite-jdbc "3.27.2.1"]
                  ]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler image-shower.core/handler
          :nrepl {:start? true}}
+  :main image-shower.main
   :profiles {:uberjar {:aot :all
                        :main image-shower.main}})
 
